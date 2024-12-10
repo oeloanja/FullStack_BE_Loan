814e398738025c7a17ea3e414b38b3f90e4199d9
cho <inbrain144@gmail.com>
2024-12-10 15:58:18 +0900

    feat : 최종 상환 완료 로직 관련 api 추가
    - group_id를 활용하여 해당하는 대출의 상태를 한꺼번에 조회하는 api 추가

--- src/main/java/com/billit/loan_service/repository/LoanStatusRepository.java
+++ src/main/java/com/billit/loan_service/repository/LoanStatusRepository.java
@@ -1,10 +1,19 @@
 package com.billit.loan_service.repository;
 
 import com.billit.loan_service.entity.LoanStatus;
+import com.billit.loan_service.enums.LoanStatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanStatusRepository extends JpaRepository<LoanStatus, Long> {
     LoanStatus findByLoanId(long loanId);
+
+    @Query("SELECT CASE WHEN COUNT(ls) > 0 THEN true ELSE false END " +
+            "FROM LoanStatus ls " +
+            "JOIN ls.loan l " +
+            "WHERE l.groupId = :groupId AND ls.status = :status")
+    boolean existsByGroupIdAndStatus(@Param("groupId") Integer groupId, @Param("status") LoanStatusType status);
 }
